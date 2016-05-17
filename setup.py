15eb22479f998f309494184b4311ec4054f89ef1
Martin Wong <martimus.wong@gmail.com>
2016-05-17 11:25:19 -0400

    updated github links in setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     name='aboardly',
     version='0.1.1',
     description='Official Aboardly API library client for python',
-    author='Serge',
+    author='Martin',
     author_email='info@aboardly.com',
     license='MIT',
-    url="https://github.com/herkyl/aboardly-python",
-    download_url="https://github.com/herkyl/aboardly-python/tarball/0.1",
+    url="https://github.com/landland/aboardly-python",
+    download_url="https://github.com/landland/aboardly-python/tarball/0.1",
     install_requires=[
         'requests >= 2.1.0'
     ],
